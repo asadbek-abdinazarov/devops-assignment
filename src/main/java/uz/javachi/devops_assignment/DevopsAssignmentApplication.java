0985220e70bf524eb06f634916c0a35dd3243711
Asadbek <asadbekabdunazarov602@gmail.com>
2026-01-17 16:57:14 +0500

    Test logs added

--- src/main/java/uz/javachi/devops_assignment/DevopsAssignmentApplication.java
+++ src/main/java/uz/javachi/devops_assignment/DevopsAssignmentApplication.java
@@ -1,13 +1,44 @@
 package uz.javachi.devops_assignment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsAssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsAssignmentApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(DevopsAssignmentApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsAssignmentApplication.class, args);
+        init();
+    }
+
+    public static void init() {
+        log.info("Devops Assignment Application started");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.info("This is a simople Spring Boot application for DevOps assignment.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.error("Test error log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+        log.warn("Test warn log message.");
+    }
 }
