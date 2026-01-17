8df373d3a629e6feaae377a8ccf1e5cd6beb3dd2
Asadbek <asadbekabdunazarov602@gmail.com>
2026-01-17 14:43:38 +0500

    Upload project

---
+++ src/test/java/uz/javachi/devops_assignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javachi.devops_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
