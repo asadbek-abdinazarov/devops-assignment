732908a8ab21ed4de6773125a159dafb450d7aab
Asadbek <asadbekabdunazarov602@gmail.com>
2026-01-18 15:07:34 +0500

    updated 1

---
+++ src/main/java/uz/javachi/devops_assignment/config/Configuration.java
@@ -0,0 +1,14 @@
+package uz.javachi.devops_assignment.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+}
--- src/main/java/uz/javachi/devops_assignment/controller/UserController.java
+++ src/main/java/uz/javachi/devops_assignment/controller/UserController.java
@@ -1,18 +1,20 @@
 package uz.javachi.devops_assignment.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping
     public ResponseEntity<?> test() {
-        return ResponseEntity.ok()
-                .body("User controller is working!");
+        log.info("Test endpoint called");
+        return ResponseEntity.ok("test");
     }
 
 }
